89db8b74a8b485dbb590257c47dad8055f752c76
Mitch Lloyd <mitch@northcoders.com>
2019-01-21 10:55:45 +0000

    add array-access challenge

---
+++ fundamentals/array-access.js
@@ -0,0 +1,31 @@
+const getFirstItem = (arr) => {
+    // write and test a function that returns the first-item in an array
+};
+
+const getLastItem = (arr) => {
+    // write a function that returns the last item of any array
+};
+
+const getOuterSum = (nums) => {
+    // returns the sum of the outer nums in an array
+    // [10,3,4,5,6] --> returns 16
+};
+
+const getNestedItem = (arr, levelOne, levelTwo) => {
+    // write a function that returns an item nested in an array
+    // e.g. const nums = [ [100,7], [3,10,33], [2,2,2,5,78], [1900,3,4,17] ]
+    // getNestedItem(nums,3,2) --> should return 
+};
+
+const getItem = (arr,position) => {
+    // returns the position in an array
+    // if the position is greater than or equal to the array length then it will loop back round
+};
+
+const getMiddleItem = (arr) => {
+    // write a function that returns the middle item of an array 
+};
+
+const accessWithTotal = (arr,positions) => {
+    // returns a new array with the positions from the 
+};

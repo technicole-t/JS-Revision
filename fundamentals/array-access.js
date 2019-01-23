f6df90840f12f51802de197ee5bfc3db6117a3ac
Mitch Lloyd <mitch@northcoders.com>
2019-01-23 11:15:00 +0000

    add object-access-3 challenge and examples

--- fundamentals/array-access.js
+++ fundamentals/array-access.js
@@ -1,3 +1,6 @@
+// ===> Array-Access <====
+// =========> PART-1 <========
+
 const getFirstItem = (arr) => {
     // write and test a function that returns the first-item in an array
 };
@@ -26,6 +29,22 @@ const getMiddleItem = (arr) => {
     // write a function that returns the middle item of an array 
 };
 
-const accessWithTotal = (arr,positions) => {
+const buildNewArray = (arr,positions) => {
     // returns a new array with the positions from the 
+    // [100, 4, 5, 6, 8, 100, 33],[0, 1, 3]
+    // return value should be [100, 4, 6] positions 0, 1 and 3 from the array
 };
+
+const accessWithString = (arr,indexString) => {
+    // accessWithString([100,35,46,[100,3,[99, 36],1,2],[2]],'3->2->1');
+};
+
+const calculateDeterminant = matrix => {
+    // a matrix is an array with two arrays nested inside, each nested array has 2 numbers
+    // an example of a matrix is [[10,3],[7,5]];
+    // determinant is value found by multiplying the first number in the first array with the second number in the second array
+    // subtract the second number in the first array multiplied by the first number in the second array
+};
+
+// =========> PART-2 <========
+

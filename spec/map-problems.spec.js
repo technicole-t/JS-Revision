a116bdd1e38e2815de8e5185c8580489c17ff11d
isabelmaccabee <isabel.maccabee@gmail.com>
2019-10-02 11:40:41 +0100

    update map-problems.spec

---
+++ spec/map-problems.spec.js
@@ -0,0 +1,14 @@
+const {
+  incrementNums,
+  multiplyNums,
+  extractPonyNames,
+  makeNameSentence,
+  getFullNames,
+  makeFilmDetails,
+  getMainDetails
+} = require("../array-methods/map-problems/challenges");
+const { expect } = require("chai");
+
+describe("incrementNums", () => {
+  it("", () => {});
+});

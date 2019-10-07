afd4abec0c9903149dab073f759948b2fe2b3372
isabelmaccabee <isabel.maccabee@gmail.com>
2019-10-07 11:02:52 +0100

    add problems for array-object mixture

--- collections/mixture/arrays-objects.js
+++ collections/mixture/arrays-objects.js
@@ -1,3 +1,14 @@
-const createPerson = () => {
-    // should 
+const addGreetingToPeople = () => {};
+
+const extractInfo = () => {};
+
+const increasePizzaCount = () => {};
+
+const updateMobileNumber = () => {};
+
+module.exports = {
+  addGreetingToPeople,
+  extractInfo,
+  increasePizzaCount,
+  updateMobileNumber
 };
---
+++ spec/array-object-problems.spec.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("array-object-problems", () => {
+  describe("addGreetingToPeople", () => {
+    it("", () => {});
+  });
+});

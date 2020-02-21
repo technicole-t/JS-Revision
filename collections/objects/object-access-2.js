e9d5d1da95798146e9bdca2fde691142c0e2db7e
technicole-t <thorleynicole@gmail.com>
2020-02-21 09:27:58 +0000

    3 katas complete with TDD, 1st test for 4th kata written and passing

--- collections/objects/object-access-2.js
+++ collections/objects/object-access-2.js
@@ -1,24 +1,37 @@
 const accessName = person => {
+  return person.name;
   // given a person object of the form { name: 'Bob', age: 42 }
   // should return 'Bob'
 };
 
 const accessObject = (obj, key) => {
+  return obj[key];
   // access the value in an object using a key
   // accessObject( { fruit: 'banana' }, 'fruit') <-- returns 'banana'
   // accessObject( { hero: 'erdos' }, 'hero') <-- 'erdos'
 };
 
 const accessNestedArray = (obj, index) => {
-  // object of the form { list: [] } <-- access something in this list
-  // i.e. accessNestedArray({list : ['hey','whats','wrong','with','you']},3) // should return 'with'
+  if (Object.values(obj) < index) return "";
+  let array = Object.values(obj);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length < index) {
+      return "";
+    } else if (array[i].length > index) {
+      return array[i][index];
+    }
+  }
 };
+// object of the form { list: [] } <-- access something in this list
+// i.e. accessNestedArray({list : ['hey','whats','wrong','with','you']},3) // should return 'with'
 
 const addProperty = (obj, keyValuePair) => {
+  if (keyValuePair.length === 0) return obj;
   // keyValuePair is an array with a key and a value
   // e.g. ["name","mitch"]
   // use this pair to add the key value pair to a new obj with all the properties of the old obj
-  // addProperty({job: 'teacher'},['name','mitch']) // should return {job: 'teacher', name: 'mitch }
+  // addProperty({job: 'teacher'},['name','mitch'])
+  // should return {job: 'teacher', name: 'mitch }
 };
 
 const removeName = person => {
--- spec/object-access.spec.js
+++ spec/object-access.spec.js
@@ -1,1 +1,75 @@
+const { expect } = require("chai");
+const {
+  accessName,
+  accessObject,
+  accessNestedArray,
+  addProperty,
+  removeName,
+  removeProperty
+} = require("../collections/objects/object-access-2");
+
+describe("accessName", () => {
+  it("returns the name when passed one person object", () => {
+    const person = { name: "Bob", age: 42 };
+    const actual = accessName(person);
+    const expected = "Bob";
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe("accessObject", () => {
+  it("when passed an object and key, returns the value of the key", () => {
+    const object = { fruit: "banana" };
+    const key = "fruit";
+    const actual = accessObject(object, key);
+    const expected = "banana";
+    expect(actual).to.equal(expected);
+  });
+  it("given an object of multiple key value pairs return the value of the key passed in as a second argument", () => {
+    const object = { fruit: "banana", name: "Nicole", holiday: "Thailand" };
+    const key = "holiday";
+    const actual = accessObject(object, key);
+    const expected = "Thailand";
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe("accessNestedArray", () => {
+  it("returns an empty string when passed an object with an empty list", () => {
+    const obj = { list: [] };
+    const index = 1;
+    const actual = accessNestedArray(obj, index);
+    const expected = "";
+    expect(actual).to.eql(expected);
+  });
+  it("returns an empty string when passed an object in which the index passed does not exist", () => {
+    const obj = { list: ["dog"] };
+    const index = 2;
+    const actual = accessNestedArray(obj, index);
+    const expected = "";
+    expect(actual).to.eql(expected);
+  });
+  it("returns the value of the list object passed when given a valid index position to search", () => {
+    const obj = { list: ["dog", "cat", "pig", "snake", "rat"] };
+    const index = 3;
+    const actual = accessNestedArray(obj, index);
+    const expected = "snake";
+    expect(actual).to.eql(expected);
+  });
+});
+
+describe.only("addProperty", () => {
+  it("when passed an empty array returns the object passed", () => {
+    const obj = { job: "teacher" };
+    const keyValuePair = [];
+    const actual = addProperty(obj, keyValuePair);
+    const expected = { job: "teacher" };
+    expect(actual).to.eql(expected);
+  });
+  //   it("when passed an array, adds it to the existing object as a key value pair', () => {
+  //     const obj = { job: "teacher" };
+  //     const keyValuePair = ['name','mitch'];
+  //   });
+});
+
 // write your tests for the object-problems here
